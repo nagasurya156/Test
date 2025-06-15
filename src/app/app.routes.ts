8072671bf1554da1fc2387b85b38b83544060340
surya <nagasurya156@gmail.com>
2025-06-15 11:02:10 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SampleCompComponent } from './components/sample-comp/sample-comp.component';
+import { SignalsComponent } from './components/signals/signals.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        redirectTo:"/Home",
+        pathMatch:"full",
+    },
+    {
+        path:"Home",
+        component: SignalsComponent,
+    }
+];
---
+++ src/app/components/sample-comp/sample-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-comp',
+  imports: [],
+  templateUrl: './sample-comp.component.html',
+  styleUrl: './sample-comp.component.scss'
+})
+export class SampleCompComponent {
+
+}
